ab045e6a06342af6c20ff14f44c3b62e38b49536
Ubuntu <ubuntu@ip-172-31-23-49.ec2.internal>
2021-03-11 19:30:37 +0000

    changes to app.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 5000
+
+app.get("/", function(req, res){
+    res.send('hello world')
+
+})
+
+app.listen(PORT, ()=>{
+    console.log('hello')
+})
